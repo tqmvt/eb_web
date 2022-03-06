757d8ddadd4bdef4f119d4e8712e09db025d96e8
Rob Cunning <creative@cunning.ca>
2022-03-05 17:04:41 -0700

    make approval an explicit step before staking actions

--- src/Components/components/MyStaking.js
+++ src/Components/components/MyStaking.js
@@ -1,10 +1,12 @@
-import React, { memo, useState } from 'react';
+import React, {memo, useEffect, useState} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setStakeCount, setVIPCount } from '../../GlobalState/User';
 import { Form, Spinner } from 'react-bootstrap';
 import { toast } from 'react-toastify';
 import { createSuccessfulTransactionToastContent } from '../../utils';
 import config from '../../Assets/networks/rpc_config.json';
+import {ethers} from "ethers";
+import {ERC721} from "../../Contracts/Abis";
 
 const MyStaking = ({ walletAddress = null }) => {
   const dispatch = useDispatch();
@@ -15,7 +17,41 @@ const MyStaking = ({ walletAddress = null }) => {
   const [isUnstaking, setIsUnstaking] = useState(false);
   const [isHarvesting, setIsHarvesting] = useState(false);
   const [amount, setAmount] = useState(1);
-  
+  const [isApproved, setIsApproved] = useState(false);
+  const [isApproving, setIsApproving] = useState(false);
+
+  const txExtras = {
+    gasPrice: ethers.utils.parseUnits('5000', 'gwei'),
+  }
+
+  // Allow exception to be thrown for other functions to catch it
+  const setApprovalForAll = async () => {
+    const isApproved = await user.membershipContract.isApprovedForAll(user.stakeContract.address, user.address);
+    if (!isApproved) {
+      let tx = await user.membershipContract.setApprovalForAll(user.stakeContract.address, true, txExtras);
+      await tx.wait();
+    }
+  };
+
+  const approve = async () => {
+    try {
+      setIsApproving(true);
+      await setApprovalForAll();
+      setIsApproved(true);
+    } catch (error) {
+      if (error.data) {
+        toast.error(error.data.message);
+      } else if (error.message) {
+        toast.error(error.message);
+      } else {
+        console.log(error);
+        toast.error('Unknown Error');
+      }
+    } finally {
+      setIsApproving(false);
+    }
+  }
+
   const stake = async () => {
     if (!user.stakeContract || amount === 0) return;
     if (amount >= vipCount) {
@@ -24,8 +60,8 @@ const MyStaking = ({ walletAddress = null }) => {
     }
     try {
       setIsStaking(true);
-      await user.membershipContract.setApprovalForAll(config.stake_contract, true, { gasPrice: 5000000000000 });
-      await user.stakeContract.stake(amount, { gasPrice: 5000000000000 });
+      if (!isApproved) await approve();
+      await user.stakeContract.stake(amount, txExtras);
       dispatch(setStakeCount(stakeCount + amount));
       dispatch(setVIPCount(vipCount - amount));
       toast.success(createSuccessfulTransactionToastContent("Successfully staked"));
@@ -69,9 +105,21 @@ const MyStaking = ({ walletAddress = null }) => {
     }    
   }
 
-  const onAmountChnage = (e) => {
+  const onAmountChange = (e) => {
     setAmount(parseInt(e.target.value));
   }
+
+  useEffect(async () => {
+    if (user.membershipContract) {
+      try {
+        const isApproved = await user.membershipContract.isApprovedForAll(user.address, user.stakeContract.address);
+        setIsApproved(isApproved);
+      } catch (e) {
+        setIsApproved(false);
+      }
+    }
+  }, [user]);
+
   return (
     <>
       <div className="text-center">
@@ -85,50 +133,68 @@ const MyStaking = ({ walletAddress = null }) => {
       </div>
       <div className="row mt-4 text-center d-flex justify-content-center">  
         <div className="col-lg-2 text-center">
-          <Form.Control type="number" placeholder="Input the amount" onChange={onAmountChnage} value={amount}/>
+          <Form.Control type="number" placeholder="Input the amount" onChange={onAmountChange} value={amount}/>
         </div>
       </div>
       <div className="row mt-4">
-        <div className='col-lg-12 d-flex justify-content-center'>
-          <button className="btn-main lead mx-5" onClick={stake} disabled={amount ===0 || vipCount === 0}>
-            {isStaking ? (
-              <>
-                Staking...
-                <Spinner animation="border" role="status" size="sm" className="ms-1">
-                  <span className="visually-hidden">Loading...</span>
-                </Spinner>
-              </>
-            ) : (
-              <>Stake</>
-            )}
-          </button>
+        {isApproved && (
+            <div className='col-lg-12 d-flex justify-content-center'>
+              <button className="btn-main lead mx-5" onClick={stake} disabled={amount ===0 || vipCount === 0}>
+                {isStaking ? (
+                    <>
+                      Staking...
+                      <Spinner animation="border" role="status" size="sm" className="ms-1">
+                        <span className="visually-hidden">Loading...</span>
+                      </Spinner>
+                    </>
+                ) : (
+                    <>Stake</>
+                )}
+              </button>
 
-          <button className="btn-main lead mx-5" onClick={unStake} disabled={amount === 0 || stakeCount === 0}>
-            {isUnstaking ? (
-              <>
-                UnStaking...
-                <Spinner animation="border" role="status" size="sm" className="ms-1">
-                  <span className="visually-hidden">Loading...</span>
-                </Spinner>
-              </>
-            ) : (
-              <>UnStake</>
-            )}
-          </button>
+              <button className="btn-main lead mx-5" onClick={unStake} disabled={amount === 0 || stakeCount === 0}>
+                {isUnstaking ? (
+                    <>
+                      UnStaking...
+                      <Spinner animation="border" role="status" size="sm" className="ms-1">
+                        <span className="visually-hidden">Loading...</span>
+                      </Spinner>
+                    </>
+                ) : (
+                    <>UnStake</>
+                )}
+              </button>
 
-          <button className="btn-main lead mx-5" onClick={harvest}>
-            {isHarvesting ? (
-              <>
-                Harvesting...
-                <Spinner animation="border" role="status" size="sm" className="ms-1">
-                  <span className="visually-hidden">Loading...</span>
-                </Spinner>
-              </>
-            ) : (
-              <>Harvest</>
-            )}
-          </button>
-        </div>
+              <button className="btn-main lead mx-5" onClick={harvest}>
+                {isHarvesting ? (
+                    <>
+                      Harvesting...
+                      <Spinner animation="border" role="status" size="sm" className="ms-1">
+                        <span className="visually-hidden">Loading...</span>
+                      </Spinner>
+                    </>
+                ) : (
+                    <>Harvest</>
+                )}
+              </button>
+            </div>
+        )}
+        {!isApproved && (
+            <div className="col-lg-12 d-flex justify-content-center">
+              <button className="btn-main lead mx-5" onClick={approve} disabled={amount ===0 || vipCount === 0}>
+                {isApproving ? (
+                    <>
+                      Approving...
+                      <Spinner animation="border" role="status" size="sm" className="ms-1">
+                        <span className="visually-hidden">Loading...</span>
+                      </Spinner>
+                    </>
+                ) : (
+                    <>Approve</>
+                )}
+              </button>
+            </div>
+        )}
       </div>
     </>  
   );
