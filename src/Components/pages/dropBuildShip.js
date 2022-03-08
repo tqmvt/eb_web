22ee6b414c1b81b192de6dd58cbd361f46a6ab5d
Robarelli <rob@ebisusbay.com>
2022-03-08 09:04:23 -0700

    few more UI changes

--- src/Components/pages/dropBuildShip.js
+++ src/Components/pages/dropBuildShip.js
@@ -11,34 +11,13 @@ import {
 } from '../../utils';
 import ShipABI from "../../Contracts/Ship.json"
 import ShipItemABI from "../../Contracts/ShipItem.json"
+import styled from "styled-components";
 
 const Drop = () => {
-  const [amount, setAmount] = useState(1);
-  const [shipType, setShipType] = useState("");
-  const [shipTypes, setShipTypes] = useState([]);
-  const [isMinting, setIsMinting] = useState(false);
+  const [ships, setShips] = useState([]);
+  const [partsBalances, setPartsBalances] = useState([]);
   const [shipContract, setShipContract] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-
-
-
-  const [regularQuantity, setRegularQuantity] = useState(0);
-  const [greatQuantity, setGreatQuantity] = useState(0);
-  const [legendaryQuantity, setLegendaryQuantity] = useState(0);
-  const onQuantityChange = (key, e) => {
-    const value = e.target.value;
-    if (key === 'regular') {
-      setRegularQuantity(value)
-    } else if (key === 'great') {
-      setGreatQuantity(value)
-    } else if (key === 'legendary') {
-      setLegendaryQuantity(value)
-    }
-  }
-
-  useEffect(() => {
-   
-  }, []);
 
   const user = useSelector((state) => {
     return state.user;
@@ -49,11 +28,12 @@ const Drop = () => {
       try {
         setIsLoading(true);
         let spaceShip = await new ethers.Contract(config.spaceship_contract, ShipABI.abi, user.provider.getSigner());
-        const ship1 = await spaceShip.SHIP1();
-        const ship2 = await spaceShip.SHIP2();
-        const ship3 = await spaceShip.SHIP3();
+        const ship1 = await spaceShip.SHIP1(); // Regular
+        const ship2 = await spaceShip.SHIP2(); // Great
+        const ship3 = await spaceShip.SHIP3(); // Legendary
         const ships = [ship1, ship2, ship3];
-console.log('ships', ships)
+        setShips(ships);
+
         const shipItemDrop = config.known_contracts.find(drop => drop.slug === "crosmocrafts-parts");
 
         let shipItem = await new ethers.Contract(shipItemDrop.address, ShipItemABI.abi, user.provider.getSigner());
@@ -62,15 +42,7 @@ console.log('ships', ships)
           const balance = await shipItem.balanceOf(user.address, i);  
           ids.push(balance.toNumber());
         }
-        console.log(ids);
-        
-        const tmpShipTypes = [];
-        for(let i = 0; i < 3; i ++) {
-          if (ids[i] > 0 && ids[i + 3] > 0 && ids[i + 6] > 0)
-          tmpShipTypes.push({name: `ship${i+1}`, value: ships[i]});
-        }
-        console.log(tmpShipTypes);
-        setShipTypes(tmpShipTypes);
+        setPartsBalances(ids);
 
         setShipContract(spaceShip);
       } catch (error) {
@@ -85,110 +57,17 @@ console.log('ships', ships)
     init();
   }, [init, user]);
 
-  
-
   const mint = async(address, quantity) => {
     if (!shipContract)  return;
     console.log('minting...', quantity, address);
     try {
-      setIsMinting(true);
       await shipContract.mint(quantity, address);
       toast.success(createSuccessfulTransactionToastContent("Successfully minted"));
     } catch(err) {
       toast.error(err.message);
     } finally {
-      setIsMinting(false);
     }
   }
-
-  const handleChange = (e) => {
-    setAmount(e.target.value);
-  }
-
-  const onChangeShipType = (e) => {
-    setShipType(e.target.value);
-  }
-
-  const ShipBuilderCard = ({type}) => {
-    let key = 0;
-    let quantity;
-    if (type === 'regular') {
-      key = 0;
-      quantity = regularQuantity;
-    } else if (type === 'great') {
-      key = 1;
-      quantity = greatQuantity;
-    } else if (type === 'legendary') {
-      key = 2;
-      quantity = legendaryQuantity;
-    }
-
-    return (
-        <div className="card eb-nft__card h-100 w-100 shadow">
-          <div className="card-body d-flex flex-column">
-            <h5>{humanize(type)} Parts</h5>
-            <div className="row row-cols-1 row-cols-sm-3 row-cols-md-4 g-3">
-              <div className="card border-0">
-                <img src={`/img/collections/crosmonauts/parts/${type}-booster.webp`} className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <h5 className="card-title">Booster</h5>
-                  <p className="card-text">Parts Collected: 4</p>
-                </div>
-              </div>
-              <div className="card border-0">
-                <img src={`/img/collections/crosmonauts/parts/${type}-engine.webp`}  className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <h5 className="card-title">Engine</h5>
-                  <p className="card-text">Parts Collected: 4</p>
-                </div>
-              </div>
-              <div className="card border-0">
-                <img src={`/img/collections/crosmonauts/parts/${type}-deck.webp`}  className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <h5 className="card-title">Deck</h5>
-                  <p className="card-text">Parts Collected: 4</p>
-                </div>
-              </div>
-              <div className="card border-0">
-                <div className="card-body d-flex">
-                  <div className="align-self-center">
-                    <h5 className="card-title d-flex justify-content-center">Build {humanize(type)} Ship</h5>
-                    <div className="row row-cols-1 g-3 mt-2">
-                      <div className="col d-flex justify-content-center">
-                        <Form.Label>Quantity</Form.Label>
-                      </div>
-                      <div className="col d-flex justify-content-center">
-                        <Form.Control
-                            type="number"
-                            placeholder="Input the amount"
-                            onChange={(e) => onQuantityChange('regular', e)}
-                            value={regularQuantity}
-                            style={{width:'100px', marginBottom: 0, appearance:'none', margin: 0}}
-                        />
-                      </div>
-                      <div className="col d-flex justify-content-center">
-                        <button className="btn-main lead mb-5 mr15" onClick={() => mint(shipTypes[key].value, quantity)}>
-                          {isMinting ? (
-                              <>
-                                Minting...
-                                <Spinner animation="border" role="status" size="sm" className="ms-1">
-                                  <span className="visually-hidden">Loading...</span>
-                                </Spinner>
-                              </>
-                          ) : (
-                              <>Mint</>
-                          )}
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-    )
-  };
 
   return (
     <>
@@ -211,14 +90,6 @@ console.log('ships', ships)
       </section>
 
       <section className="container">
-        <div className="row row-cols-1 g-4">
-          <ShipBuilderCard type="regular" />
-          <ShipBuilderCard type="great" />
-          <ShipBuilderCard type="legendary" />
-        </div>
-      </section>
-
-        <div className='container' style={{"marginTop": "120px"}}>
         {
           isLoading ? (
             <div className="row mt-4" style={{"marginTop": "220px"}}>
@@ -227,51 +98,153 @@ console.log('ships', ships)
               </div>
             </div>
           ):
-          (<>
-          <div className='row d-flex justify-content-center'>
-            <div className='col-lg-4'>
-              <Form.Select aria-label="Default select example" onChange={onChangeShipType}>
-                {
-                  shipTypes.length === 0 ? <option>No available </option>
-                  : <>
-                    <option>Select the ShipType</option>
-                    {
-                      shipTypes.map((ship, index) => {
-                          return (
-                            <option value={ship.value} key={`${index}_${ship.value}`}>{ship.name} </option>
-                          )
-                      })
-                    }
-                  </>
-                }
-            </Form.Select>
-            </div>
-            <div className='col-lg-4'>
-              <Form.Control type="number" placeholder="Input the amount" onChange={handleChange} value={amount}/>
-            </div>
-          </div>
-
-          <div className='row d-flex justify-content-center'>
-            <button className="btn-main lead mb-5 mr15" onClick={mint}>
-              {isMinting ? (
-                <>
-                  Minting...
-                  <Spinner animation="border" role="status" size="sm" className="ms-1">
-                    <span className="visually-hidden">Loading...</span>
-                  </Spinner>
-                </>
-              ) : (
-                <>Mint</>
+          (
+            <div className="row row-cols-1 g-4">
+              {ships[0] && (
+                  <ShipBuilderCard
+                      type="regular"
+                      shipAddress={ships[0]}
+                      key={0}
+                      mintCallback={mint}
+                      quantityCollected={[partsBalances[0], partsBalances[3], partsBalances[6]]}
+                  />
               )}
-            </button>
-          </div>
-          </>)
+              {ships[1] && (
+                  <ShipBuilderCard
+                      type="great"
+                      shipAddress={ships[1]}
+                      key={1}
+                      mintCallback={mint}
+                      quantityCollected={[partsBalances[1], partsBalances[4], partsBalances[7]]}
+                  />
+              )}
+              {ships[2] && (
+                  <ShipBuilderCard
+                      type="legendary"
+                      shipAddress={ships[2]}
+                      key={2}
+                      mintCallback={mint}
+                      quantityCollected={[partsBalances[2], partsBalances[5], partsBalances[8]]}
+                  />
+              )}
+            </div>
+          )
         }
-        </div>
- 
-      
+      </section>
       <Footer />
     </>
   );
 };
 export default Drop;
+
+const GreyscaleImg = styled.img`
+  -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
+  filter: grayscale(100%);
+`;
+
+const ShipBuilderCard = ({type, shipAddress, key, mintCallback, quantityCollected}) => {
+  const [isMinting, setIsMinting] = useState(false);
+  const [quantity, setQuantity] = useState(0);
+
+  const onQuantityChange = (key, e) => {
+    const value = e.target.value;
+    const maxAvailable = Math.min(...quantityCollected);
+    console.log(value, quantityCollected, maxAvailable)
+    if (value <= maxAvailable && value > 0) {
+        setQuantity(value);
+    }
+  }
+
+  const onMint = async() => {
+    setIsMinting(true);
+    await mintCallback(shipAddress, quantity);
+    setIsMinting(false);
+  };
+
+  useEffect(() => {
+      const maxAvailable = Math.min(...quantityCollected);
+      setQuantity(maxAvailable > 0 ? 1 : 0);
+  }, []);
+
+  return (
+      <div className="card eb-nft__card h-100 w-100 shadow">
+        <div className="card-body d-flex flex-column">
+          <h5>{humanize(type)} Parts</h5>
+          <div className="row row-cols-1 row-cols-sm-3 row-cols-md-4 g-3">
+            <div className="card border-0">
+              {quantityCollected[0] > 0 ? (
+                <img src={`/img/collections/crosmonauts/parts/${type}-engine.webp`}  className="card-img-top" alt="..." />
+              ) : (
+                <GreyscaleImg src={`/img/collections/crosmonauts/parts/${type}-engine.webp`}  className="card-img-top" alt="..." />
+              )}
+              <div className="card-body">
+                <h5 className="card-title">Core</h5>
+                <p className="card-text">Parts Collected: {quantityCollected[0]}</p>
+              </div>
+            </div>
+            <div className="card border-0">
+              {quantityCollected[0] > 0 ? (
+                <img src={`/img/collections/crosmonauts/parts/${type}-booster.webp`}  className="card-img-top" alt="..." />
+              ) : (
+                <GreyscaleImg src={`/img/collections/crosmonauts/parts/${type}-booster.webp`}  className="card-img-top" alt="..." />
+              )}
+              <div className="card-body">
+                <h5 className="card-title">Booster</h5>
+                <p className="card-text">Parts Collected: {quantityCollected[1]}</p>
+              </div>
+            </div>
+            <div className="card border-0">
+              {quantityCollected[0] > 0 ? (
+                <img src={`/img/collections/crosmonauts/parts/${type}-deck.webp`}  className="card-img-top" alt="..." />
+              ) : (
+                <GreyscaleImg src={`/img/collections/crosmonauts/parts/${type}-deck.webp`}  className="card-img-top" alt="..." />
+              )}
+              <div className="card-body">
+                <h5 className="card-title">Deck</h5>
+                <p className="card-text">Parts Collected: {quantityCollected[2]}</p>
+              </div>
+            </div>
+            <div className="card border-0">
+              <div className="card-body d-flex">
+                <div className="align-self-center">
+                  <h5 className="card-title d-flex justify-content-center">Build {humanize(type)} Ship</h5>
+                    {quantityCollected[0] > 0 &&  quantityCollected[1] > 0 && quantityCollected[2] > 0 ? (
+                        <div className="row row-cols-1 g-3 mt-2">
+                            <div className="col d-flex justify-content-center">
+                                <Form.Label>Quantity</Form.Label>
+                            </div>
+                            <div className="col d-flex justify-content-center">
+                                <Form.Control
+                                    type="number"
+                                    placeholder="Input the amount"
+                                    onChange={(e) => onQuantityChange('regular', e)}
+                                    value={quantity}
+                                    style={{width:'100px', marginBottom: 0, appearance:'none', margin: 0}}
+                                />
+                            </div>
+                            <div className="col d-flex justify-content-center">
+                                <button className="btn-main lead mb-5 mr15" onClick={() => onMint(shipAddress, quantity)}>
+                                    {isMinting ? (
+                                        <>
+                                            Minting...
+                                            <Spinner animation="border" role="status" size="sm" className="ms-1">
+                                                <span className="visually-hidden">Loading...</span>
+                                            </Spinner>
+                                        </>
+                                    ) : (
+                                        <>Mint</>
+                                    )}
+                                </button>
+                            </div>
+                        </div>
+                    ) : (
+                        <p>Not enough parts</p>
+                    )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  )
+};
