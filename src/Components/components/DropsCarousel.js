b9420dce245dfd99284931b889e0f0eedb1eb7db
Robarelli <rob@ebisusbay.com>
2022-04-09 19:34:05 -0600

    fix carousel formatting for array costs

--- src/Components/components/DropsCarousel.js
+++ src/Components/components/DropsCarousel.js
@@ -260,22 +260,30 @@ export default class Responsive extends Component {
                         <div className="d-attr">
                           <div className="col">
                             <span className="d-title">Mint Price</span>
-                            {Array.isArray(drop.cost)?(<h3>{drop.cost.join(', ')} CRO</h3>):
-                            (<h3>{ethers.utils.commify(drop.cost)} CRO</h3>)}
+                            {Array.isArray(drop.cost) ?
+                              (<h3>{ethers.utils.commify(Math.min(...drop.cost.map(c => parseInt(c))))} - {ethers.utils.commify(Math.max(...drop.cost.map(c => parseInt(c))))} CRO</h3>):
+                              (<h3>{ethers.utils.commify(drop.cost)} CRO</h3>)
+                            }
                             {drop.erc20Cost && drop.erc20Unit && (
                               <h3>
                                 {ethers.utils.commify(drop.erc20Cost)} {drop.erc20Unit}
                               </h3>
                             )}
-                            {drop.memberCost && (Array.isArray(drop.memberCost) ? (<h5>Members: {drop.memberCost.join(', ')} CRO</h5>):
-                            (<h5>Members: {ethers.utils.commify(drop.memberCost)} CRO</h5>))}
+                            {drop.memberCost && (
+                              Array.isArray(drop.memberCost) ?
+                                (<h5>Members: {ethers.utils.commify(Math.min(...drop.memberCost.map(c => parseInt(c))))} - {ethers.utils.commify(Math.max(...drop.memberCost.map(c => parseInt(c))))} CRO</h5>):
+                                (<h5>Members: {ethers.utils.commify(drop.memberCost)} CRO</h5>))
+                            }
                             {drop.erc20MemberCost && drop.erc20Unit && (
                               <h5>
                                 Members: {ethers.utils.commify(drop.erc20MemberCost)} {drop.erc20Unit}
                               </h5>
                             )}
-                            {drop.whitelistCost && (Array.isArray(drop.whitelistCost) ? (<h5>Members: {drop.memberCost.join(', ')} CRO</h5>):
-                            (<h5>Whitelist: {ethers.utils.commify(drop.whitelistCost)} CRO</h5>))}
+                            {drop.whitelistCost &&
+                              (Array.isArray(drop.whitelistCost) ?
+                                (<h5>Whitelist: {ethers.utils.commify(Math.min(...drop.memberCost.map(c => parseInt(c))))} - {ethers.utils.commify(Math.max(...drop.memberCost.map(c => parseInt(c))))} CRO</h5>):
+                                (<h5>Whitelist: {ethers.utils.commify(drop.whitelistCost)} CRO</h5>))
+                            }
                             {drop.specialWhitelistCost && (
                               <h5>Special Whitelist: {ethers.utils.commify(drop.specialWhitelistCost)} CRO</h5>
                             )}
