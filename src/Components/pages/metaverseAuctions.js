d3519c5c8e92ba36c2808c393328affe85023970
Tommi Laukkanen <tommi.laukkanen@weare.fi>
2022-04-25 11:20:39 +0300

    WEB-1345 UI - Update textual content on Landing page

--- src/Components/pages/metaverseAuctions.js
+++ src/Components/pages/metaverseAuctions.js
@@ -14,7 +14,7 @@ const MetaverseAuctions = () => {
         <div className="row">
           <div className="col-lg-12">
             <div className="text-center pt-5">
-              <h2>Metaverse Auction for Ukraine</h2>
+              <h2>Auctions</h2>
             </div>
           </div>
           <div className="col-lg-6 pt-3">
@@ -26,7 +26,7 @@ const MetaverseAuctions = () => {
             </div>
           </div>
           <div className="col-lg-6 text-center align-middle d-flex align-items-center">
-            <div className="heading mt-3">All proceeds from this auction will be donated to victims of the Ukranian conflict. Either place you bids below, or enter the metaverse and place your bids there.</div>
+            <div className="heading mt-3">Join our auction in the metaverse, or just make a bid below.</div>
           </div>
         </div>
       </section>
