b4ec593ed89f1527ae1a87dd7fccc9178605eb40
Robarelli <rob@ebisusbay.com>
2022-05-01 16:30:44 -0600

    link name of offer maker to seller page

--- src/utils.js
+++ src/utils.js
@@ -190,6 +190,8 @@ export function siPrefixedNumber(num) {
 }
 
 export function shortAddress(address) {
+  if (!address) return '';
+  
   return `${address.substring(0, 4)}...${address.substring(address.length - 3, address.length)}`;
 }
 
