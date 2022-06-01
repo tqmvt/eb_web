778c403644f554eabaa5459915faba775947aa6e
Robarelli <rob@ebisusbay.com>
2022-06-01 12:02:00 -0600

    use cns lookup on nft721 pages for seller name

--- src/Components/pages/nft721.js
+++ src/Components/pages/nft721.js
@@ -347,6 +347,7 @@ const Nft721 = ({ address, id }) => {
                         type="Seller"
                         address={currentListing.seller}
                         to={`/seller/${currentListing.seller}`}
+                        useCnsLookup={true}
                       />
                     )}
 
