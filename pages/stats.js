e6e53847640cf84995239646b007fd78a7bbf627
Robarelli <rob@ebisusbay.com>
2022-07-06 15:30:34 -0600

    add details to competition tab

--- pages/stats.js
+++ pages/stats.js
@@ -11,6 +11,9 @@ import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons
 import styles from '../src/Components/Leaderboard/styles.module.scss';
 import PageHead from "../src/Components/Head/PageHead";
 import Footer from "../src/Components/components/Footer";
+import {Badge} from "react-bootstrap";
+import { Modal, ModalTitle } from 'react-bootstrap';
+import styled from "styled-components";
 
 export const carouselSettings = {
   infinite: true,
@@ -77,10 +80,21 @@ const headers = {
   biggestSingleSale: ['User', 'Total Volume'],
 };
 
+const StyledModal = styled(Modal)`
+  .modal-content {
+    background: ${({ theme }) => theme.colors.bgColor1};
+  }
+`;
+
+const StyledModalTitle = styled(ModalTitle)`
+  color: ${({ theme }) => theme.colors.textColor3};
+`;
+
 export default function Stats() {
+  const dispatch = useDispatch();
   const [timeframe, setTimeframe] = useState('custom');
   const [type, setType] = useState('totalVolume');
-  const dispatch = useDispatch();
+  const [showDialog, setShowDialog] = useState(false);
 
   const leaderBoard = useSelector((state) => {
     return state.leaderBoard;
@@ -144,6 +158,14 @@ export default function Stats() {
             </ul>
           </div>
         </div>
+        {timeframe === 'custom' && (
+          <div>
+            <p>
+              Prizes up for grabs for the top 5 ranked wallets in each category! Competition runs from July 1st - 31st. &nbsp;
+              <Badge bg="primary" onClick={() => setShowDialog(true)} className="cursor-pointer">More Info</Badge>
+            </p>
+          </div>
+        )}
         <div className="d-flex gap-3 mt-lg-4 align-items-center justify-content-between">
           <div className={`nft ${styles.dots}`}>
             <Slider {...carouselSettings} prevArrow={<PrevArrow />} nextArrow={<NextArrow />}>
@@ -186,6 +208,42 @@ export default function Stats() {
         <p className="text-center small"><a href="https://cdn.ebisusbay.com/Contest_Terms_and_Conditions.html">Contest Terms and
           Conditions</a></p>
       }
+
+      <StyledModal show={showDialog} onHide={() => setShowDialog(false)}>
+        <Modal.Header>
+          <StyledModalTitle>Competition Details</StyledModalTitle>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Based on leaderboard results from July 1st - 31st, Ebisu's Bay will be awarding prizes in the following structure: </p>
+
+          <span className="fw-bold">Total Volume</span>
+          <ul>
+            <li>1st - 8,000 CRO</li>
+            <li>2nd - 5,000 CRO</li>
+            <li>3rd - 3,000 CRO</li>
+            <li>4th - 2,000 CRO</li>
+            <li>5th - 2,000 CRO</li>
+          </ul>
+
+          <span className="fw-bold">Total Buy, Sell and Biggest Single Sale</span>
+          <ul>
+            <li>1st - 4,000 CRO</li>
+            <li>2nd - 2,500 CRO</li>
+            <li>3rd - 1,500 CRO</li>
+            <li>4th - 1,000 CRO</li>
+            <li>5th - 1,000 CRO</li>
+          </ul>
+
+          <p>So get trading, climbing and finding your way into the prize money!</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <button className="p-4 pt-2 pb-2 btn_menu inline white lead" onClick={() => setShowDialog(false)}>
+            Close
+          </button>
+        </Modal.Footer>
+      </StyledModal>
+
       <Footer />
     </div>
   );
